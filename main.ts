58e583929cc9415e540def849ab0d871f32be16c
Jerome Su <suchuanxj@gmail.com>
2023-01-05 13:27:26 +0800

    version 1.0.0 release

--- main.ts
+++ main.ts
@@ -1,137 +1,70 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin } from "obsidian";
 
-// Remember to rename these classes and interfaces!
+export default class CodeTab extends Plugin {
+    async onload() {
+        this.registerMarkdownPostProcessor((element,context) => {
+            var toggleTab:number = element.getElementsByClassName('language-tab').length
+            if(toggleTab === 0) return
+            
+            //create basic skeleton of tab-container
+            element.className = 'tab-container'
+            var tabsTag:HTMLElement = document.createElement('ul')
+            tabsTag.className='tabs'
+            var contentsTag = document.createElement('div')
+            contentsTag.className='tab-contents'
+            element.appendChild(tabsTag)
+            element.appendChild(contentsTag)
+            var codeTag = element.querySelector('code')
+            if(codeTag !== null) {
+                var codeTagText = codeTag.innerHTML
+                var codeTagTextArray = codeTagText.split('lang:')
+                for(var i = 1 ; i < codeTagTextArray.length; i++) {
+                    // fill up tabs
+                    var language = codeTagTextArray[i].substring(0,codeTagTextArray[i].indexOf('\n'))
+                    var tabItemTag = document.createElement('li')
+                    tabItemTag.className = 'tab-item'
+                    tabItemTag.innerHTML = language
+                    if(i===1) tabItemTag.addClass('tab-item--active')
+                    tabItemTag.onclick = (e) => handler(e)
+                    tabsTag.appendChild(tabItemTag);
+                    
+                    //fill up tab-contents
+                    var codeText = codeTagTextArray[i].substring(codeTagTextArray[i].indexOf('\n')+1)
+                    var newCodeTag = document.createElement('code')
+                    newCodeTag.className = 'language-'+language
+                    newCodeTag.innerHTML = codeText
+                    var buttonTag = document.createElement('button')
+                    buttonTag.className = 'copy-code-button'
+                    buttonTag.innerHTML = 'Copy'
+                    var preTag = document.createElement('pre')
+                    preTag.className = 'language-'+language
+                    preTag.appendChild(newCodeTag)
+                    preTag.appendChild(buttonTag)
+                    var tabContentTag = document.createElement('div')
+                    tabContentTag.className = 'tab-content'
+                    if(i===1) tabContentTag.addClass('tab-content--active')
+                    tabContentTag.appendChild(preTag)
+                    contentsTag.appendChild(tabContentTag)
+                }
 
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+            }
+            // event handler
+            var tabItemTags = element.getElementsByClassName('tab-item') 
+            var tabContentTags = element.getElementsByClassName('tab-content')
+            const handler = (e:MouseEvent) => {
+                for(var i = 0; i < tabItemTags.length; i++) {
+                    if(tabItemTags[i] === e.srcElement) {
+                        tabItemTags[i].classList.add('tab-item--active');
+                        tabContentTags[i].classList.add('tab-content--active');
+                    } else {
+                        tabItemTags[i].classList.remove('tab-item--active');
+                        tabContentTags[i].classList.remove('tab-content--active');
+                    }
+                    
+                }
+            }
+            // remove old codes
+            if(element.firstChild !== null) element.removeChild(element.firstChild)
+        })
+    }
 }
