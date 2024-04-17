99dc8d44bd820a0da2ac0f0cf2a071a21caceb35
Huajin <x2585045489p@outlook.com>
2024-04-17 14:12:52 +0800

    1. Fill LICENSE 2. Update the icon: circle-plus -> plus, strike -> strikethrough.

--- src/components/tabeditor.ts
+++ src/components/tabeditor.ts
@@ -87,8 +87,7 @@ export class TabEditor {
 
     const strikeButton = document.createElement("button");
     strikeButton.addClass("toolbar-button", "strike-button");
-    addIcon("strike", '<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-strikethrough"><path d="M16 4H9a3 3 0 0 0-2.83 4"/><path d="M14 12a4 4 0 0 1 0 8H6"/><line x1="4" x2="20" y1="12" y2="12"/></svg>');
-    setIcon(strikeButton, "strike");
+    setIcon(strikeButton, "strikethrough");
     strikeButton.onclick = () => {
       editor.dispatch({
         changes: {
--- src/components/tabnavbutton.ts
+++ src/components/tabnavbutton.ts
@@ -20,9 +20,7 @@ export class TabNavButton {
   createTabNavButtonEl(): HTMLElement {
     const addButton = document.createElement('div');
     addButton.className = "tab-nav-button";
-    const circleplus = `<svg xmlns="http://www.w3.org/2000/svg" width="96" height="96" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-plus"><circle cx="12" cy="12" r="10"/><path d="M8 12h8"/><path d="M12 8v8"/></svg>`;
-    addIcon("circle-plus", circleplus);
-    setIcon(addButton, "circle-plus");
+    setIcon(addButton, "plus");
     return addButton;
   }
 }
